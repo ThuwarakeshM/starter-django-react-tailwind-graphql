9fca92d782a1c33038e1f6e38d7a88c1b535002a
thuwarakeshm <thuwarakeshm@stax.com>
2021-08-31 19:45:25 +0530

    initial commit

---
+++ web/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex w-full min-h-screen items-center justify-center">
+      <h1 className="text-2xl text-bold text-blue-900">
+        Hi from Thwarakesh Murallie
+      </h1>
+    </div>
+  );
+}
+
+export default App;
